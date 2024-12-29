7751a691c740cecb745df6204fb244de94368cba
Techy0Ganga <151946977+Techy0Ganga@users.noreply.github.com>
2024-12-29 21:56:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 $(".printable").on("click", function () {
-    var sound = new Audio("https://github.com/Techy0Ganga/Calculator/raw/refs/heads/master/click.wav");
+    var sound = new Audio("https://github.com/Techy0Ganga/Calculator/raw/refs/heads/master/click.wav?raw=true");
     sound.play();
     $(".display").val($(".display").val() + $(this).text());
 });
 
 $(".clear").on("click", function () {
-    var sound = new Audio("click.wav");
+    var sound = new Audio("https://github.com/Techy0Ganga/Calculator/raw/refs/heads/master/click.wav?raw=true");
     sound.play();
     $(".display").val("");
 });
 
 $(".backspace").on("click", function () {
-    var sound = new Audio("click.wav");
+    var sound = new Audio("https://github.com/Techy0Ganga/Calculator/raw/refs/heads/master/click.wav?raw=true");
     sound.play();
     var value = $(".display").val();
     $(".display").val(value.slice(0, -1));
@@ -22,7 +22,7 @@ $(".backspace").on("click", function () {
 
 
 $(".equals").on("click", function () {
-        var sound = new Audio("click.wav");
+        var sound = new Audio("https://github.com/Techy0Ganga/Calculator/raw/refs/heads/master/click.wav?raw=true");
         sound.play();
         var expression = $(".display").val();
         console.log(expression);
