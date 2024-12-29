00ee6ac24a8722485f803314049305d5168df82a
Techy0Ganga <aswingangaofficial@gmail.com>
2024-12-29 21:43:34 +0530

    added sound effect

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 
 $(".printable").on("click", function () {
+    var sound = new Audio("click.mp3");
+    sound.play();
     $(".display").val($(".display").val() + $(this).text());
 });
 
 $(".clear").on("click", function () {
+    var sound = new Audio("click.mp3");
+    sound.play();
     $(".display").val("");
 });
 
 $(".backspace").on("click", function () {
+    var sound = new Audio("click.mp3");
+    sound.play();
     var value = $(".display").val();
     $(".display").val(value.slice(0, -1));
 });
@@ -16,6 +22,8 @@ $(".backspace").on("click", function () {
 
 
 $(".equals").on("click", function () {
+        var sound = new Audio("click.mp3");
+        sound.play();
         var expression = $(".display").val();
         console.log(expression);
         var result = math.evaluate(expression);
